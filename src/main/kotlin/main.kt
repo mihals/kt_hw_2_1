b3de0e4165c7b35116818789ac56fcea8c4ade33
mihals <mihalsnes@gmail.com>
2022-03-28 10:59:59 +0300

    Remake HW

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,11 @@
 package ru.netology
 
 fun main() {
-    val amount: Int = 35_25
+    val amount: Int = 3500_25
     val comission: Int
 
-    if (amount * 0.75 > 3500) {
-        comission = (3500 * 0.75).toInt()
+    if (amount * 0.0075 > 3500) {
+        comission = (amount * 0.0075).toInt()
     } else {
         comission = 3500
     }
